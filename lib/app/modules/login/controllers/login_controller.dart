1f209018c0bbc32715fc04de9c6a6a0c10956c72
Mauricio Rojas <mavro@MacBook-Pro-Development.local>
2022-09-11 21:10:10 -0300

    feat: add login controller and methods

--- lib/app/modules/login/controllers/login_controller.dart
+++ lib/app/modules/login/controllers/login_controller.dart
@@ -1,23 +1,13 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class LoginController extends GetxController {
-  //TODO: Implement LoginController
-
-  final count = 0.obs;
-  @override
-  void onInit() {
-    super.onInit();
-  }
-
-  @override
-  void onReady() {
-    super.onReady();
-  }
+  var checkBox = false.obs;
+  TextEditingController phoneController = TextEditingController();
 
   @override
   void onClose() {
+    phoneController.dispose();
     super.onClose();
   }
-
-  void increment() => count.value++;
 }
--- lib/app/modules/login/views/login_view.dart
+++ lib/app/modules/login/views/login_view.dart
@@ -10,170 +10,198 @@ class LoginView extends GetView<LoginController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        children: [
-          Container(
-            alignment: Alignment.centerLeft,
-            height: 150,
-            child: Image.asset(
-              "assets/logo/logo-login.png",
-              fit: BoxFit.contain,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 30,
+        ),
+        child: ListView(
+          children: [
+            SizedBox(
+              height: 20,
             ),
-          ),
-          SizedBox(
-            height: 30,
-          ),
-          Text(
-            "Por favor ingrese con su número de Teléfono",
-          ),
-          Text(
-            "Número HP",
-          ),
-          TextField(
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              hintText: "Cth. 08129011xxxx",
+            Container(
+              alignment: Alignment.centerLeft,
+              height: 150,
+              child: Image.asset(
+                "assets/logo/logo-login.png",
+                fit: BoxFit.contain,
+              ),
             ),
-          ),
-          Row(
-            children: [
-              Checkbox(
-                value: false,
-                onChanged: (value) {},
-              ),
-              Expanded(
-                child: RichText(
-                  text: TextSpan(
-                      text: "Acepto los  ",
-                      style: TextStyle(
-                        color: Colors.black,
-                      ),
-                      children: [
-                        // refactor
-                        TextSpan(
-                          recognizer: TapGestureRecognizer()
-                            ..onTap = () {
-                              print("Términos");
-                            },
-                          text: "términos",
-                          style: TextStyle(
-                            color: Colors.red,
-                          ),
-                        ),
-                        TextSpan(
-                          recognizer: TapGestureRecognizer()
-                            ..onTap = () {
-                              print("Condiciones");
-                            },
-                          text: ", condiciones ",
-                          style: TextStyle(
-                            color: Colors.red,
-                          ),
-                        ),
-                        TextSpan(
-                          recognizer: TapGestureRecognizer()
-                            ..onTap = () {
-                              print("Privacidad");
-                            },
-                          text: "y privacidad ",
-                          style: TextStyle(
-                            color: Colors.red,
-                          ),
-                        ),
-                        TextSpan(
-                          text: "de uso. ",
-                          style: TextStyle(
-                            color: Colors.black,
-                          ),
-                        ),
-                      ]),
-                ),
-              ),
-            ],
-          ),
-          ElevatedButton(
-            onPressed: () {},
-            child: Text(
-              "INGRESAR",
+            SizedBox(
+              height: 30,
+            ),
+            Text(
+              "Por favor ingrese con su número de Teléfono",
               style: TextStyle(
-                color: Color(0xFF747D8C),
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
               ),
             ),
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Colors.grey[300],
+            SizedBox(
+              height: 30,
             ),
-          ),
-          Center(
-            child: Text("O inicia sesión usando"),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              OutlinedButton(
-                onPressed: () {},
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Image.asset("assets/icons/facebook-icons.png"),
-                    SizedBox(
-                      width: 10,
-                    ),
-                    Text(
-                      "Facebook",
-                      style: TextStyle(
-                        color: Color(0xFF3B5998),
-                      ),
-                    ),
-                  ],
-                ),
-                style: ButtonStyle(
-                    fixedSize: MaterialStateProperty.all(
-                      Size(150, 50),
-                    ),
-                    shape: MaterialStateProperty.all(
-                      RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(4.0)),
-                    ),
-                    side: MaterialStateProperty.all(
-                      BorderSide(
-                        color: Color(0xFF3B5998),
-                      ),
-                    )),
+            Text(
+              "Número HP",
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
               ),
-              OutlinedButton(
-                onPressed: () {},
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Image.asset("assets/icons/twitter-icons.png"),
-                    SizedBox(
-                      width: 10,
-                    ),
-                    Text(
-                      "Twitter",
-                      style: TextStyle(
-                        color: Color(0xFF1DA1F2),
-                      ),
-                    ),
-                  ],
-                ),
-                style: ButtonStyle(
-                    fixedSize: MaterialStateProperty.all(
-                      Size(150, 50),
-                    ),
-                    shape: MaterialStateProperty.all(
-                      RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(4.0)),
-                    ),
-                    side: MaterialStateProperty.all(
-                      BorderSide(
-                        color: Color(0xFF1DA1F2),
-                      ),
-                    )),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            TextField(
+              controller: controller.phoneController,
+              autocorrect: false,
+              keyboardType: TextInputType.phone,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: "CL. +56 9 12345xxxx",
               ),
-            ],
-          )
-        ],
+            ),
+            Row(
+              children: [
+                Obx(
+                  () => Checkbox(
+                    activeColor: Colors.red,
+                    value: controller.checkBox.value,
+                    onChanged: (value) => controller.checkBox.toggle(),
+                  ),
+                ),
+                Expanded(
+                  child: RichText(
+                    text: TextSpan(
+                        text: "Acepto los  ",
+                        style: TextStyle(
+                          color: Colors.black,
+                        ),
+                        children: [
+                          // refactor
+                          TextSpan(
+                            recognizer: TapGestureRecognizer()
+                              ..onTap = () {
+                                print("Términos");
+                              },
+                            text: "términos",
+                            style: TextStyle(
+                              color: Colors.red,
+                            ),
+                          ),
+                          TextSpan(
+                            recognizer: TapGestureRecognizer()
+                              ..onTap = () {
+                                print("Condiciones");
+                              },
+                            text: ", condiciones ",
+                            style: TextStyle(
+                              color: Colors.red,
+                            ),
+                          ),
+                          TextSpan(
+                            recognizer: TapGestureRecognizer()
+                              ..onTap = () {
+                                print("Privacidad");
+                              },
+                            text: "y privacidad ",
+                            style: TextStyle(
+                              color: Colors.red,
+                            ),
+                          ),
+                          TextSpan(
+                            text: "de uso. ",
+                            style: TextStyle(
+                              color: Colors.black,
+                            ),
+                          ),
+                        ]),
+                  ),
+                ),
+              ],
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text(
+                "INGRESAR",
+                style: TextStyle(
+                  color: Color(0xFF747D8C),
+                ),
+              ),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.grey[300],
+              ),
+            ),
+            Center(
+              child: Text("O inicia sesión usando"),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                OutlinedButton(
+                  onPressed: () {},
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset("assets/icons/facebook-icons.png"),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Text(
+                        "Facebook",
+                        style: TextStyle(
+                          color: Color(0xFF3B5998),
+                        ),
+                      ),
+                    ],
+                  ),
+                  style: ButtonStyle(
+                      fixedSize: MaterialStateProperty.all(
+                        Size(150, 50),
+                      ),
+                      shape: MaterialStateProperty.all(
+                        RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(4.0)),
+                      ),
+                      side: MaterialStateProperty.all(
+                        BorderSide(
+                          color: Color(0xFF3B5998),
+                        ),
+                      )),
+                ),
+                OutlinedButton(
+                  onPressed: () {},
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset("assets/icons/twitter-icons.png"),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Text(
+                        "Twitter",
+                        style: TextStyle(
+                          color: Color(0xFF1DA1F2),
+                        ),
+                      ),
+                    ],
+                  ),
+                  style: ButtonStyle(
+                      fixedSize: MaterialStateProperty.all(
+                        Size(150, 50),
+                      ),
+                      shape: MaterialStateProperty.all(
+                        RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(4.0)),
+                      ),
+                      side: MaterialStateProperty.all(
+                        BorderSide(
+                          color: Color(0xFF1DA1F2),
+                        ),
+                      )),
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
